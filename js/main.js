fd46749d6132caac4021c59d1ddf74481154e839
ImNotAGay <43098725+iLoveGachimuchi@users.noreply.github.com>
2019-01-31 15:52:06 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -7,14 +7,13 @@ const tk = new t_keyHook;
 const dm = new t_dragManager;
 
 
-//cmd.sessionSave = true;
+cmd.sessionSave = false;
 tc.hidden = true;
 var command = (c) => {
   cmd.c(c);
 }
 
 tc.draw("body");
-//tc.hideConsole();
 
 tk.registryKeyHook(
   function () {
@@ -34,6 +33,39 @@ tk.registryKeyHook(
 
 dm.onDragCancel = function (dragObject) {};
 dm.onDragEnd = function (dragObject, dropElem) {};
+
+
+
+var recorder;
+
+function startrecord() {
+  startUserMedia();
+}
+
+function startUserMedia() {
+  recorder = new Recorder(microfonesource);
+  recorder && recorder.record();
+}
+
+function stoprecord() {
+  recorder && recorder.stop();
+  alert("Stopped recording.");
+  recorder && recorder.exportWAV(function (blob) {
+    var url = URL.createObjectURL(blob);
+    var li = document.createElement("li");
+    var au = document.createElement("audio");
+    var hf = document.createElement("a");
+    au.controls = true;
+    au.src = url;
+    hf.href = url;
+    hf.download = new Date().toISOString() + ".wav";
+    hf.innerHTML = hf.download;
+    li.appendChild(au);
+    li.appendChild(hf);
+    recordingslist.appendChild(li);
+  });
+}
+
 
 window.onload = function () {
 
